ba3111c83da5cd2e8b84d4ea8402bff223ef2c14
Alex Alan Nunes <alexalannunes@gmail.com>
2021-05-12 16:33:15 -0300

    fix: set mode: no co-cors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,14 @@
 const key = process.env.REACT_APP_API_KEY;
 const city_id = 443871;
-const format = process.env.NODE_ENV === "production" ? "json" : "json-cors";
+const format = "json-cors";
 
 console.log(process.env);
 const req = () =>
   fetch(
-    `https://api.hgbrasil.com/weather?woeid=${city_id}&key=${key}&format=${format}`
+    `https://api.hgbrasil.com/weather?woeid=${city_id}&key=${key}&format=${format}`,
+    {
+      mode: "no-cors",
+    }
   ).then((r) => r.json());
 
 export { req };
