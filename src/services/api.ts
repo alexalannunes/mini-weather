631ba7ad39cc13a824af43734906a03a5355514c
Alex Alan Nunes <alexalannunes@gmail.com>
2021-05-12 15:59:29 -0300

    refactor: format on env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,11 @@
 const key = process.env.REACT_APP_API_KEY;
 const city_id = 443871;
+const format = process.env.NODE_ENV === "production" ? "json" : "json-cors";
 
 console.log(process.env);
 const req = () =>
   fetch(
-    `https://api.hgbrasil.com/weather?woeid=${city_id}&key=${key}&format=json-cors`
+    `https://api.hgbrasil.com/weather?woeid=${city_id}&key=${key}&format=${format}`
   ).then((r) => r.json());
 
 export { req };
