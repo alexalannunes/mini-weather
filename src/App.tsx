b6669d1aa6adfcc0b06b65290cc165ab3baffcba
Alex Alan Nunes <alexalannunes@gmail.com>
2021-05-15 11:51:22 -0300

    ci: add offline alert; animations and UI

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ interface IWeatherState {
     min: number;
     weekday: string;
   }>;
+  loaded: false;
 }
 
 function App() {
@@ -23,16 +24,8 @@ function App() {
     city: "Pereiro, CE",
     description: "",
     wind_speedy: "",
-    forecast: [
-      // {
-      //   condition: "",
-      //   date: "",
-      //   description: "",
-      //   max: "",
-      //   min: "",
-      //   weekday: "",
-      // },
-    ],
+    forecast: [],
+    loaded: false,
   });
 
   React.useEffect(() => {
@@ -45,66 +38,123 @@ function App() {
       ?.querySelector('[name="theme-color"]')
       ?.setAttribute("content", theme.background);
 
-    req()
-      .then((e) => {
-        setW(e.results);
-        // localStorage.setItem("tm", JSON.stringify(e));
-      })
-      .catch((error) => {
-        console.dir(error);
-      });
+    if (navigator.onLine) {
+      req()
+        .then((e) => {
+          setW({ ...e.results, loaded: true });
+          localStorage.setItem("tm", JSON.stringify(e));
+        })
+        .catch((error) => {
+          ab();
+        });
+    } else {
+      const div = document.createElement("div");
+      div.textContent = "Offline: mostrando dados do cache";
+      div.className = "popup-alert";
+      document.body.appendChild(div);
 
-    // setW(JSON.parse(localStorage.getItem("tm") || "{}").results || {});
+      setTimeout(function () {
+        document.body.removeChild(div);
+      }, 2000);
+
+      ab();
+    }
+
+    // setTimeout(function () {
+    //   setW(
+    //     {
+    //       ...JSON.parse(localStorage.getItem("tm") || "{}").results,
+    //       loaded: true,
+    //     } || {}
+    //   );
+    // }, 1000);
+
+    function ab() {
+      setTimeout(function () {
+        setW(
+          {
+            ...JSON.parse(localStorage.getItem("tm") || "{}").results,
+            loaded: true,
+          } || {}
+        );
+      }, 1000);
+    }
   }, []);
 
   return (
     <div className="W">
-      <div className="W__top fadeIn">
-        <span className="W__city">{w.city}</span>
-      </div>
-      <div className="W__center">
-        <img src={theme.icon} alt="moon" />
-        <h1 className="W__degree">{w.temp}º</h1>
-        {w.description && <h4 className="W__state">{w.description}</h4>}
-        {w.wind_speedy && (
-          <>
-            <h6 className="W__wind">Vento</h6>
-            <span className="W__velocity">{w.wind_speedy}</span>
-          </>
-        )}
-      </div>
-      <div className="W__bottom">
-        {w.forecast.length ? (
-          <div className="W__forecast" role="list">
-            {w.forecast.slice(0, 6).map((i, x) => (
-              <div
-                data-date={i.date}
-                role="listitem"
-                key={x}
-                className="W-forecast__item"
-              >
-                <span>{i.weekday}</span>
-                <span className="W-forecast__item-opacity">{i.condition}</span>
-                <span>
-                  {i.min}º <span className="W-forecast__item-opacity">~</span>{" "}
-                  {i.max}º
-                </span>
+      {w.loaded ? (
+        <>
+          <div className="W__top animate__animated animate__fadeInUp">
+            <span className="W__city">{w.city}</span>
+          </div>
+          <div className="W__center animate__animated animate__fadeInUp animate__delay-center">
+            <>
+              <img src={theme.icon} alt="moon" />
+              <h1 className="W__degree animate__animated animate__fadeInUp">
+                {w.temp}º
+              </h1>
+              {w.description && <h4 className="W__state">{w.description}</h4>}
+              {w.wind_speedy && (
+                <>
+                  <h6 className="W__wind">Vento</h6>
+                  <span className="W__velocity">{w.wind_speedy}</span>
+                </>
+              )}
+            </>
+          </div>
+          <div className="W__bottom animate__animated animate__fadeInUp animate__delay-bottom">
+            {w.forecast.length ? (
+              <div className="W__forecast" role="list">
+                {w.forecast.slice(0, 6).map((i, x) => (
+                  <div
+                    data-date={i.date}
+                    role="listitem"
+                    key={x}
+                    className="W-forecast__item"
+                  >
+                    <span>{i.weekday}</span>
+                    <span className="W-forecast__item-opacity">
+                      {i.condition}
+                    </span>
+                    <span>
+                      <span style={{ opacity: 0.7 }}>{i.min}º</span>{" "}
+                      <span className="W-forecast__item-opacity">~</span>{" "}
+                      {i.max}º
+                    </span>
+                  </div>
+                ))}
               </div>
-            ))}
+            ) : (
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  flex: 1,
+                }}
+              >
+                <h1 style={{ fontSize: 30 }}>¯\_(ツ)_/¯</h1>
+              </div>
+            )}
           </div>
-        ) : (
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              flex: 1,
-            }}
-          >
-            <h1 style={{ fontSize: 30 }}>¯\_(ツ)_/¯</h1>
+        </>
+      ) : (
+        <div
+          style={{
+            flex: 1,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div className="loader">
+            <svg className="circular-loader" viewBox="25 25 50 50">
+              <circle className="loader-path" cx="50" cy="50" r="20"></circle>
+            </svg>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
